9d4d70eef4410a3a25ff4d1e41d127d8b06ccebc
Ray C Horn <raychorn@gmail.com>
2020-10-29 02:33:24 +0000

    adding more stuff to test firebase database known as firestore.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCWmphhcv1ZTOvgTAHh0A6dz5QCqMRk37o",
+    authDomain: "fir-project1-8dac5.firebaseapp.com",
+    databaseURL: "https://fir-project1-8dac5.firebaseio.com",
+    projectId: "fir-project1-8dac5",
+    storageBucket: "fir-project1-8dac5.appspot.com",
+    messagingSenderId: "323231490676",
+    appId: "1:323231490676:web:ff1c3933a276954473a721",
+    measurementId: "G-L0ZBL2T9F4"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -14,3 +23,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
